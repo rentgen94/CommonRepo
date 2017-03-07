c846d501377ee502056f2263027bff0652b7270c
Egor <rentgen.94@mail.ru>
2017-03-08 01:08:22 +0300

    Change structure of repo. Add new projects. I want make this repo for common projects

---
+++ Studing_for_AlgorithmsInJava/FirstProgramm/java_m.bat
@@ -0,0 +1,9 @@
+@echo off
+
+:: ***************************************************
+:: java-algs4
+:: ------------------
+:: Wrapper for java that includes algs4.jar
+:: ***************************************************
+
+java -cp ".;.\out\production\FirstProgramm\;C:\Users\Western-Co\algs4\stdlib.jar" %*
---
+++ Studing_for_AlgorithmsInJava/FirstProgramm/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * Created by Western-Co on 27.11.2016.
+ */
+public class Main {
+    static Scanner scanner;
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This is first arg: " + scanner.nextLine());
+    }
+}
